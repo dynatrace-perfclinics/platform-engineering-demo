02957f59da71edccfcced50d003325d9275c2a89
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-05-09 11:15:54 +1000

    Pytest OTEL export only supports gRPC
    From: https://pypi.org/project/pytest-opentelemetry/
    
    "Only the OTLP over gRPC exporter is currently supported."

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -4,5 +4,5 @@
 # 2. Run test harness
 export OTEL_SERVICE_NAME=codespace-platform
 export PYTEST_RUN_NAME=startup-automated-test
-export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4318
+export OTEL_EXPORTER_OTLP_ENDPOINT=http://localhost:4317
 pytest --export-traces codespaces_test.py
